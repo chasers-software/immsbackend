1ada5e06b083e29747ad2b0f28dc22186d61a0cb
Bijay Shrestha <shresthabijay26@yahoo.com>
2020-12-16 21:56:37 +0545

    git

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,9 +1,9 @@
 const mysql=require('mysql2/promise');
 const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'imms1',
+    host:'imm.crsmbpoqueg2.us-east-1.rds.amazonaws.com',
+    user:'admin',
+    password:'ImmsDatabase',
+    database:'imms',
     waitForConnections:true
 });
 
