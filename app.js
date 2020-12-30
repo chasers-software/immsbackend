3d18b379fcaa5c10ee3109ea70524f8d5eb1f926
Bijay Shrestha <shresthabijay26@yahoo.com>
2020-12-30 14:47:40 +0545

    no pass

--- app.js
+++ app.js
@@ -44,12 +44,7 @@ app.use('/api',limiter);
 app.use(express.json({limit:'10kb'}));
 app.use(cookieParser());
 
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
-    next();
-});
+
 app.use(xss());
 app.use(hpp());
 
