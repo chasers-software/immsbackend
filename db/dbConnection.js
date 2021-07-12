d62723d9d3b986eee25b684991ec6497478faccf
Sandeep Acharya <acharyas378@gmail.com>
2021-07-12 08:59:01 +0545

    modified for using on both local and online

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -12,17 +12,20 @@ const mysql=require('mysql2/promise');
 const pool=mysql.createPool({
     host:process.env.dbhost||'localhost',
     user:process.env.dbuser||'root',
-    port:3306,
+    port:process.env.dbport||3306,
     password:process.env.dbpassword||'',
     database:process.env.dbname||'imms',
-    //waitForConnections:true
+    waitForConnections: process.env.waitForConnections||false,
 });
+
+if(!process.env.waitForConnections){
   pool.getConnection(function(err) {
-    if(err) {
-      console.log('error '+err.message);
-    }
-    console.log('Connected to the MySQL server.');
-  });
+      if(err) {
+        console.log('error '+err.message);
+      }
+      console.log('Connected to the MySQL server.');
+    });
+}
 
 module.exports=pool;
 
